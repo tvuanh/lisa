3d754da7c70b42c0d9904fcfbabd93958cc5bfbf
Anh Tuan Vu <avu@united.domain>
2019-04-10 17:27:24 +0200

    Used encoders and decodes in class QTable.

--- src/lisa/rl.py
+++ src/lisa/rl.py
@@ -27,17 +27,17 @@ class Translator(object):
 
 class QTable(object):
 
-    actions_space = range(20)
-
-    def __init__(self, gamma=1., minvisits=10):
+    def __init__(self, actions, states, gamma=1., minvisits=10):
+        self.actions_translator = Translator(actions)
+        self.states_translator = Translator(states)
         # discount factor
         self.gamma = gamma
         # min number of visits per state-action
         self.minvisits = minvisits
         # Q(state, action) sample mean value
         self.Qmean = np.zeros(
-            (6, len(self.actions_space))
-            ) # 6 state-rows, 20 action-columns
+            (len(self.states_translator), len(self.actions_translator))
+            ) # state-rows, action-columns
         # number of visits per state-action
         self.visits = np.ones(self.Qmean.shape)
         # sum of squared future rewards
@@ -46,44 +46,58 @@ class QTable(object):
         self.Qvar = np.ones(self.Qmean.shape) * np.inf
 
     def predict(self, state):
-        visits = self.visits[state, :]
+        encoded_state = states_translator(state)
+        encoded_actions = self.actions_translator.encoders.values()
+        visits = self.visits[encoded_state, :]
         if np.min(visits) < self.minvisits:
-            action = np.random.choice(self.actions_space)
+            encoded_action = np.random.choice(encoded_actions)
         else:
-            means, variances = self.Qmean[state, :], self.Qvar[state, :]
+            means = self.Qmean[encoded_state, :]
+            variances = self.Qvar[encoded_state, :]
             sigmas = np.sqrt(np.divide(variances, visits))
-            draws = means + sigmas * np.random.randn(len(self.actions_space))
-            action = self.action_from_values(draws)
-        return decode_action(action)
-
-    def action_from_values(self, values):
-        maxQstate = np.max(values)
-        possible_actions = [a for a in self.actions_space if values[a] >= maxQstate]
-        return np.random.choice(possible_actions)
+            draws = means + sigmas * np.random.randn(len(encoded_actions))
+            encoded_action = self.action_from_values(draws)
+        return self.actions_translator.decode(encoded_action)
 
     def fit(self, state, action, reward, next_state):
-        future_reward = reward + self.gamma * np.max(self.Qmean[next_state, :])
-        encoded_action = encode_action(action)
+        encoded_state = self.states_translator.encode(state)
+        encoded_action = self.actions_translator.encode(action)
+        encoded_next_state = self.states_translator.encode(next_state)
+        future_reward = reward + self.gamma * np.max(
+            self.Qmean[encoded_next_state, :])
         # update mean, sum squared rewards and variance in exact order
-        self.update_mean(state, encoded_action, future_reward)
-        self.update_sum_squared_rewards(state, encoded_action, future_reward)
-        self.update_variance(state, encoded_action)
-        self.visits[state, encoded_action] += 1
+        self.update_mean(encoded_state, encoded_action, future_reward)
+        self.update_sum_squared_rewards(
+            encoded_state, encoded_action, future_reward)
+        self.update_variance(encoded_state, encoded_action)
+        self.visits[encoded_state, encoded_action] += 1
 
     def update_mean(self, state, action, future_reward):
-        visits = self.visits[state, action]
-        Qm = self.Qmean[state, action]
-        self.Qmean[state, action] += (future_reward - Qm) / visits
+        encoded_state = self.states_translator.encode(state)
+        encoded_action = self.actions_translator.encode(action)
+        visits = self.visits[encoded_state, encoded_action]
+        Qm = self.Qmean[encoded_state, encoded_action]
+        self.Qmean[encoded_state, encoded_action] += (future_reward - Qm) / visits
 
     def update_sum_squared_rewards(self, state, action, future_reward):
-        self.Sr2[state, action] += future_reward * future_reward
+        encoded_state = self.states_translator.encode(state)
+        encoded_action = self.actions_translator.encode(action)
+        self.Sr2[encoded_state, encoded_action] += future_reward * future_reward
 
     def update_variance(self, state, action):
-        visits = self.visits[state, action]
+        encoded_state = self.states_translator.encode(state)
+        encoded_action = self.actions_translator.encode(action)
+        visits = self.visits[encoded_state, encoded_action]
         if visits > 1:
-            sr2 = self.Sr2[state, action]
-            Qm = self.Qmean[state, action]
+            sr2 = self.Sr2[encoded_state, encoded_action]
+            Qm = self.Qmean[encoded_state, encoded_action]
             self.Qvar[state, action] = min(
                 (sr2 - visits * Qm * Qm) / (visits - 1),
                 np.inf
             )
+
+    def action_from_values(self, values):
+        maxQstate = np.max(values)
+        encoded_actions = self.actions_translator.encoders.values()
+        possible_actions = [a for a in encoded_actions if values[a] >= maxQstate]
+        return np.random.choice(possible_actions)
