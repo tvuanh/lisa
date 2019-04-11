b5104b2316f6b7ec120c59d0ac77a929427c1bc6
Anh Tuan Vu <avu@united.domain>
2019-04-11 15:53:06 +0200

    Added integration test.

--- tests/test_rl.py
+++ tests/test_rl.py
@@ -1,4 +1,9 @@
 import unittest
+from collections import deque
+
+import numpy as np
+
+import gym
 
 from lisa import rl
 
@@ -25,3 +30,48 @@ class TestTranslator(unittest.TestCase):
     def test_decode_invalid(self):
         with self.assertRaises(ValueError):
             self.translator.decode(3)
+
+
+class TestIntegration(unittest.TestCase):
+
+    def test_integration(self):
+        episodes = 3000
+        nplays = 10
+        results = np.array([play_copy_v0(episodes) for _ in range(nplays)])
+        success = results < episodes
+        self.assertTrue(np.sum(success) > 0.7 * nplays)
+        self.assertTrue(np.mean(results[success]) < 1300)
+
+
+def play_copy_v0(episodes=3000):
+   env = gym.make('Copy-v0')
+
+   states = range(6)
+   actions = tuple(
+       [(i, j, k) for i in (0, 1) for j in (0, 1) for k in range(5)]
+   )
+   Qtable = rl.QTable(states=states, actions=actions, gamma=0.8)
+
+   performance = deque(maxlen=100)
+   performance.append(0.)
+
+   episode = 0
+   while episode < episodes and np.mean(performance) < 25.:
+       episode += 1
+       state = env.reset()
+
+       steps, rewards, done = 0, [], False
+       while not done:
+           steps += 1
+           action = Qtable.predict(state)
+           next_state, reward, done, _ = env.step(action)
+            # use shifted reward to update the Q table
+           Qtable.fit(state, action, reward + 0.5, next_state)
+           rewards.append(reward)
+           state = next_state
+       performance.append(np.sum(rewards))
+       # print(
+       #     "episode {} steps {} rewards {} total {}".format(episode, steps, rewards, np.sum(rewards))
+       # )
+
+   return episode
